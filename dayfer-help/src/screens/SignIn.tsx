3954950ef49a634a70ffda6555d08755bcc22ecd
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-11-21 01:18:52 -0300

    configuração svg e fins

--- dayfer-help/src/screens/SignIn.tsx
+++ dayfer-help/src/screens/SignIn.tsx
@@ -1,13 +1,15 @@
 import { VStack, Heading } from "native-base" ;
 
-export function SignIn() {
- return (
-  <VStack flex={1} alignItems="Center" bg="gray.600" px={8} pt={24}>
+import Logo from "../assets/logo_primary.svg";
 
-    <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-      Acesse sua Conta
-    </Heading>
-    
+export function SignIn() {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>    
   </VStack>
  )
 }
