178757e64a24f46e717ad518269154fdc8d0fe5f
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-11-21 00:54:28 -0300

    add fonte

--- dayfer-help/App.tsx
+++ dayfer-help/App.tsx
@@ -1,9 +1,22 @@
-import { } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
 
 import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loanding";
 
 export default function App() {
+  const [fontsLoaded] = useFonts( { Roboto_400Regular, Roboto_700Bold});
+
   return (
-   <SignIn/>
+    <NativeBaseProvider theme={THEME}>
+    <StatusBar
+    barStyle="light-content"
+    backgroundColor="transparent"
+    translucent
+    />
+     { fontsLoaded ? <SignIn /> : <Loading /> }
+   </NativeBaseProvider>
   );
 }
--- dayfer-help/src/screens/SignIn.tsx
+++ dayfer-help/src/screens/SignIn.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native" ;
+import { VStack, Heading } from "native-base" ;
 
 export function SignIn() {
-return (
-  <View>
-    <Text>Olá, Dayvyson Almeida.</Text>
-  </View>
+ return (
+  <VStack flex={1} alignItems="Center" bg="gray.600" px={8} pt={24}>
+
+    <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+      Acesse sua Conta
+    </Heading>
+    
+  </VStack>
  )
 }
